8351436c3c1ff1831cef1782e8f65bb73ebb983b
iBekirov <ilja.bekirov@gmail.com>
2019-08-19 23:26:21 +0300

    Execute simple query

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new('test_db.sqlite')
+db = SQLite3::Database.new('test_db')
+
+db.execute"INSERT INTO Cars (Name, Price) VALUES ('Jaguar', 7777)"
+db.close
